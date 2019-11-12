19d5dbdbdab933d9cfa3e8c5e7ab490b4e1cf5ca
Jhan C. Navarro <jnavarro@indotel.net.do>
2019-11-12 09:52:31 -0400

    retuning

--- index.php
+++ index.php
@@ -133,37 +133,7 @@
     }
   }
 
-  function sen(){
-    $to = 'jnavarro@centroindotel.gob.do';
-    $firstname = $_POST["nombre"];
-    $subject= "Foro Turismo Digital";
-    $email= $_POST["correo"];
-    $text= "Hola esto es una prueba";
-    
-
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  </td>
-        </tr>
-        <tr><td>Subject: '.$subject.'</td></tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>Email: '.$text.'</td></tr>
-        
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo 'El mensaje ha sido enviado.';
-    }else{
-        echo 'Ha ocurrido un error.';
-    }
-  }
-    
+  
     
 ?>
 </body>
