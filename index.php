8bf34fbbe9475dea88559d255d65d628fb289443
Jhan Navarro <jhannavarro@Jhans-MacBook-Pro.local>
2019-11-12 22:45:13 -0400

    done

--- index.php
+++ index.php
@@ -237,7 +237,6 @@ if(isset($_POST['confirm'])){
                 echo 'Could not run query: ' . mysql_error();
                 exit;
               }
-aa
               if ($result->num_rows > 0) {
                 $row = $result->fetch_assoc();
                 $nombre = $row["NOMBRE"];
